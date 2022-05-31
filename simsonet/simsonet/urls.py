65330e0b8c73d8a5f4c68b3b4cd1d602f3c20620
Kestutis Januskevicius <infohata@gmail.com>
2022-05-31 11:43:50 +0300

    base template with index view

--- simsonet/simsonet/urls.py
+++ simsonet/simsonet/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('', include('simsonet_site.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
