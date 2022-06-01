80ef09128f079d4062f4002312c0c8d918ab4ba3
Kestutis Januskevicius <infohata@gmail.com>
2022-06-01 10:56:42 +0300

    model str and Meta, fixes and default admins

--- simsonet/simsonet_posts/models.py
+++ simsonet/simsonet_posts/models.py
@@ -13,14 +13,22 @@ class Wall(models.Model):
     )
     name = models.CharField(_('name'), max_length=255)
     pin_to_top = models.BooleanField(_('pin to top'), default=False)
-    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return f'{self.name} {_("of")} {str(self.owner)}'
+
+    class Meta:
+        verbose_name = _('wall')
+        verbose_name_plural = _('walls')
+        ordering = ('pin_to_top', 'created_at', )
 
 
 class Post(models.Model):
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name='walls',
+        related_name='posts',
         verbose_name=_('owner'),
     )
     content = HTMLField(_('content')),
@@ -33,8 +41,8 @@ class Post(models.Model):
         blank=True,
     )
     pin_to_top = models.BooleanField(_('pin to top'), default=False)
-    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
-    updated_at = models.DateTimeField(_('updated at'), auto_add=True)
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(_('updated at'), auto_now=True, db_index=True)
     reply_to = models.ForeignKey(
         "Post", 
         verbose_name=_("reply_to"), 
@@ -51,3 +59,11 @@ class Post(models.Model):
         null=True,
         blank=True,
     )
+
+    def __str__(self):
+        return f'{str(self.created_at)}, {str(self.owner)}'
+
+    class Meta:
+        verbose_name = _('post')
+        verbose_name_plural = _('posts')
+        ordering = ('pin_to_top', 'created_at', )
